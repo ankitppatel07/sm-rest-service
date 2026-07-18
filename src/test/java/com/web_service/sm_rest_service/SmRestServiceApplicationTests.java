a59b459b8a037f4059f5d6ee087ce99fed24b5d7
Ankit <ankitppatel07@gmail.com>
2026-07-18 16:22:37 -0400

    Initialized Spring Boot project

---
+++ src/test/java/com/web_service/sm_rest_service/SmRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web_service.sm_rest_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
